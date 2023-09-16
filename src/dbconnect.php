28929a13fa0be2947a48f50abf0f6f9a99676bcf
Yuma Tsukakoshi <yttn9984kawa@gmail.com>
2023-09-17 02:54:40 +0900

    fix: dbconnnect.php

--- src/dbconnect.php
+++ src/dbconnect.php
@@ -1,11 +1,18 @@
 <?php
+class Database
+{
+  private static $db;
 
-/* ドライバ呼び出しを使用して MySQL データベースに接続する */
-$dsn = 'mysql:dbname=posts;host=db';
-$user = 'root';
-$password = 'root';
+  static function get()
+  {
+    if (!isset(self::$db))
+      self::$db = new PDO('mysql:dbname=harbors;host=db;', 'root', 'root');
+    self::$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    self::$db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 
-$pdo = new PDO($dsn, $user, $password);
-
+    return self::$db;
+  }
+}
 
 ?>
+

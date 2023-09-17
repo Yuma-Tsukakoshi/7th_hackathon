7fd9916f864aec0a85f98a86ad5982d7496239ff
Yuma Tsukakoshi <yttn9984kawa@gmail.com>
2023-09-17 10:43:50 +0900

    add: calendar.js

--- src/user/index.php
+++ src/user/index.php
@@ -1,1 +1,21 @@
-<!-- イントロページ作成場所　フロント -->
+<!-- イントロページ作成場所 フロント -->
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js" defer></script>
+  <script src="../assets/js/calender.js" defer></script>
+  <title>イントロページ</title>
+</head>
+
+<body>
+  <h1>イントロページ</h1>
+  <section class="calendar-section">
+    
+  </section>
+
+</body>
+
+</html>

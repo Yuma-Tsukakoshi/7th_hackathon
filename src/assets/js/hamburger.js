82df0bb840c16bc26f7d4b5f51016a66f5a3bc1c
haruharumaru <paris3939rusk@icloud.com>
2023-09-17 14:44:41 +0900

    ハンバーガーメニュー途中　ばつボタンの回転実装まだ

--- src/assets/js/hamburger.js
+++ src/assets/js/hamburger.js
@@ -6,9 +6,12 @@ const menuButton = document.getElementById("js-menu-closebtn");
 // メニューグループ要素を取得
 const menuGroup = document.getElementById("js-Menu-Group");
 
+// メニュー要素を取得
+const menuList = document.getElementById("js-menulist");
+
 // メニューボタンがクリックされたときの処理を定義
 menuButton.addEventListener('click', () => {
   // メニューグループのactiveクラスをトグルする
-    console.log('クリックされました');
     menuGroup.classList.toggle('active');
+    menuList.classList.toggle('active');
 });

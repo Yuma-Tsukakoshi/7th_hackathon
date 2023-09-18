5efe102b78eb982014e1afcfa21fd941fc68127b
Yuma Tsukakoshi <yttn9984kawa@gmail.com>
2023-09-18 09:15:38 +0900

    add:イベント詳細

--- src/event/event_info/event_disp.php
+++ src/event/event_info/event_disp.php
@@ -1,22 +1,20 @@
 <!-- ユーザー詳細 フロント -->
-
 <?php
 
 require_once(dirname(__FILE__) . '/../../dbconnect.php');
-require_once(dirname(__FILE__) . '/../../admin/invalid_count.php');
-require_once(dirname(__FILE__) . '/../../admin/invalid_exam_count.php');
+// イベント情報取得
+$sql = "SELECT * FROM event WHERE id = :eid";
+$stmt = $pdo->prepare($sql);
+$stmt->bindValue(':eid', $_GET['id']);
+$event = $stmt->execute();
 
-$sql1 = "SELECT * FROM users WHERE id = :id ";
-$stmt1 = $pdo->prepare($sql1);
-$stmt1->bindValue(":id", $_REQUEST["id"]);
-$stmt1->execute();
-$user = $stmt1->fetch();
+// イベント参加者情報の取得
+$sql = "SELECT * FROM user_event_relation WHERE event_id = :eid INNER JOIN users ON user_event_relation.user_id = users.id";
+$stmt = $pdo->prepare($sql);
+$stmt->bindValue(':eid', $eid);
+$stmt->execute();
+$event_users = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-$sql2 = "SELECT clients.service_name ,relation.valid FROM user_register_client as relation INNER JOIN clients ON relation.client_id = clients.client_id WHERE user_id = :id";
-$stmt2 = $pdo->prepare($sql2);
-$stmt2->bindValue(":id", $_REQUEST["id"]);
-$stmt2->execute();
-$agents = $stmt2->fetchAll();
 ?>
 
 <!DOCTYPE html>
@@ -29,265 +27,115 @@ $agents = $stmt2->fetchAll();
   <link rel="stylesheet" href="../../vendor/tailwind/tailwind.output.css">
   <link rel="stylesheet" href="../../admin/admin.css">
   <link rel="stylesheet" href="../assets/styles/badge.css">
-  <title>学生情報詳細</title>
+  <title>イベント詳細</title>
 </head>
 
 <body>
   <div class="flex h-screen bg-gray-50" :class="{ 'overflow-hidden': isSideMenuOpen}">
-    <!-- side banner -->
-    <aside class="z-20 flex-shrink-0 hidden w-64 overflow-y-auto bg-slate-500 md:block">
-      <div class="py-4 text-gray-500">
-        <a class="ml-6 text-lg font-bold text-gray-800 " href="#">
-          SideBanner
-        </a>
-        <ul class="mt-6">
-          <li class="relative px-6 py-3">
-            <a class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800" href="../../admin/boozer_index.php">
-              <span class="ml-4">企業一覧</span>
-            </a>
-          </li>
-          <li class="relative px-6 py-3">
-            <div class="notifier new">
-              <div class="badge num"><?= $exam[0]['COUNT(*)'] ?></div>
-            </div>
-            <a class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800" href="http://localhost:8080/admin/boozer_agent_exam.php">
-              <span class="ml-4">企業申請一覧</span>
-            </a>
-          </li>
-          <li class="relative px-6 py-3">
-            <a class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800" href="../../admin/boozer_student.php">
-              <span class="ml-4">学生一覧</span>
-            </a>
-          </li>
-          <li class="relative px-6 py-3">
-            <div class="notifier new">
-              <div class="badge num"><?= $count[0]['COUNT(*)'] ?></div>
-            </div>
-            <a class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800" href="../../admin/invalid_student.php">
-              <span class="ml-4">無効申請一覧</span>
-            </a>
-          </li>
-        </ul>
-      </div>
-    </aside>
+    <div class="">
+      <table class="w-3/4 text-sm text-left text-gray-500 dark:text-gray-400 mx-auto my-28 table">
+        <tbody>
+          <tr class="bg-white border-b dark:bg-gray-900 dark:border-gray-700 ">
+            <th scope="row" class="px-2 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white text-xl w-1/2">
+              イベント名
+            </th>
+            <td class="px-6 py-4 text-xl w-1/2">
+              <?= $event['name'] ?>
+            </td>
+          </tr>
+          <tr class="bg-white">
+            <th scope="row" class="px-2 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white text-l">
+              あだ名
+            </th>
+            <td class="px-6 py-4 text-l">
+              <?= $event[0]['nickname'] ?>
+            </td>
+          </tr>
 
-    <div class="flex flex-col flex-1 w-full">
-      <main class="h-full pb-16 overflow-y-auto">
-        <h1 class="my-6 text-2xl font-semibold text-gray-700 text-center">学生情報詳細 <?= $user["name"] ?> 様</h1>
-        <p class="my-6 mx-8 text-3xl font-semibold text-gray-700 flex justify-center  "><span class="head-title">学生情報 : </span><a href="http://localhost:8080/user/user_info/user_edit.php?id=<?= $user["id"] ?>" class="edit_btn">編集</a></p>
-        <div class="my-8 flex justify-center">
-          <table class="w-full mx-8 max-w-4xl bg-white shadow-md rounded-lg overflow-hidden">
-            <thead class="bg-blue-500 text-white">
-              <tr>
-                <th scope="col" class="px-6 py-3 text-left text-lg  font-medium uppercase tracking-wider">
-                  申請企業一覧
-                </th>
-                <th scope="col" class="px-6 py-3 text-left text-lg font-medium uppercase tracking-wider">
-                  無効申請判定
-                </th>
-              </tr>
-            </thead>
-            <tbody class="divide-y divide-gray-200">
-              <?php foreach ($agents as $agent) { ?>
-                <tr>
-                  <td class="px-6 py-4 whitespace-nowrap">
-                    <div class="text-ms font-medium text-gray-900">
-                      <?= $agent["service_name"] ?>
-                    </div>
-                  </td>
-                  <td class="px-6 py-4 whitespace-nowrap">
-                    <div class="text-ms font-medium text-gray-900">
-                      <?php
-                      if ($agent["valid"] == 0) {
-                        print_r("申請なし");
-                      } elseif ($agent["valid"] == 1) {
-                        print_r("申請中");
-                      } elseif ($agent["valid"] == 2) {
-                        print_r("申請承認");
-                      } elseif ($agent["valid"] == 3) {
-                        print_r("申請拒否");
-                      }
-                      ?>
-                    </div>
-                  </td>
-                </tr>
-              <?php } ?>
-            </tbody>
-          </table>
-        </div>
-        <div class="flex justify-center">
-          <table class="w-full mx-8 max-w-4xl bg-white shadow-md rounded-lg overflow-hidden">
-            <thead class="bg-blue-500 text-white">
-              <tr>
-                <th scope="col" class="px-6 py-3 text-left text-lg  font-medium uppercase tracking-wider">
-                  学生情報
-                </th>
-                <th scope="col" class="px-6 py-3 text-left text-lg font-medium uppercase tracking-wider">
-                  データ
-                </th>
-              </tr>
-            </thead>
-            <tbody class="divide-y divide-gray-200">
-              <tr>
-                <td class="px-6 py-4 whitespace-nowrap">
-                  <div class="text-ms font-medium text-gray-900">
-                    お問い合わせ日時
-                  </div>
-                </td>
-                <td class="px-6 py-4 whitespace-nowrap">
-                  <div class="text-ms font-medium text-gray-900">
-                    <?= $user["created_at"] ?>
-                  </div>
-                </td>
-              </tr>
-              <tr>
-                <td class="px-6 py-4 whitespace-nowrap">
-                  <div class="text-ms font-medium text-gray-900">
-                    氏名
-                  </div>
-                </td>
-                <td class="px-6 py-4 whitespace-nowrap">
-                  <div class="text-ms font-medium text-gray-900">
-                    <?= $user["name"] ?>
-                  </div>
-                </td>
-              </tr>
-              <tr>
-                <td class="px-6 py-4 whitespace-nowrap">
-                  <div class="text-ms font-medium text-gray-900">
-                    フリガナ
-                  </div>
-                </td>
-                <td class="px-6 py-4 whitespace-nowrap">
-                  <div class="text-ms font-medium text-gray-900">
-                    <?= $user["hurigana"] ?>
-                  </div>
-                </td>
-              </tr>
-              <tr>
-                <td class="px-6 py-4 whitespace-nowrap">
-                  <div class="text-ms font-medium text-gray-900">
-                    性別
-                  </div>
-                </td>
-                <td class="px-6 py-4 whitespace-nowrap">
-                  <div class="text-ms font-medium text-gray-900">
-                    <?= $user["sex"] ?>
-                  </div>
-                </td>
-              </tr>
-              <tr>
-                <td class="px-6 py-4 whitespace-nowrap">
-                  <div class="text-ms font-medium text-gray-900">
-                    生年月日
-                  </div>
-                </td>
-                <td class="px-6 py-4 whitespace-nowrap">
-                  <div class="text-ms font-medium text-gray-900">
-                    <?= $user["birthday"] ?>
-                  </div>
-                </td>
-              </tr>
-              <tr>
-                <td class="px-6 py-4 whitespace-nowrap">
-                  <div class="text-ms font-medium text-gray-900">
-                    メールアドレス
-                  </div>
-                </td>
-                <td class="px-6 py-4 whitespace-nowrap">
-                  <div class="text-ms font-medium text-gray-900">
-                    <?= $user["mail"] ?>
-                  </div>
-                </td>
-              </tr>
-              <tr>
-                <td class="px-6 py-4 whitespace-nowrap">
-                  <div class="text-ms font-medium text-gray-900">
-                    電話番号
-                  </div>
-                </td>
-                <td class="px-6 py-4 whitespace-nowrap">
-                  <div class="text-ms font-medium text-gray-900">
-                    <?= $user["phone"] ?>
-                  </div>
-                </td>
-              </tr>
-              <tr>
-                <td class="px-6 py-4 whitespace-nowrap">
-                  <div class="text-ms font-medium text-gray-900">
-                    所在地
-                  </div>
-                </td>
-                <td class="px-6 py-4 whitespace-nowrap">
-                  <div class="text-ms font-medium text-gray-900">
-                    <?= $user["prefecture"] ?>
-                  </div>
-                </td>
-              </tr>
-              <tr>
-                <td class="px-6 py-4 whitespace-nowrap">
-                  <div class="text-ms font-medium text-gray-900">
-                    大学
-                  </div>
-                </td>
-                <td class="px-6 py-4 whitespace-nowrap">
-                  <div class="text-ms font-medium text-gray-900">
-                    <?= $user["college"] ?>
-                  </div>
-                </td>
-              </tr>
-              <tr>
-                <td class="px-6 py-4 whitespace-nowrap">
-                  <div class="text-ms font-medium text-gray-900">
-                    学部
-                  </div>
-                </td>
-                <td class="px-6 py-4 whitespace-nowrap">
-                  <div class="text-ms font-medium text-gray-900">
-                    <?= $user["faculty"] ?>
-                  </div>
-                </td>
-              </tr>
-              <tr>
-                <td class="px-6 py-4 whitespace-nowrap">
-                  <div class="text-ms font-medium text-gray-900">
-                    学科
-                  </div>
-                </td>
-                <td class="px-6 py-4 whitespace-nowrap">
-                  <div class="text-ms font-medium text-gray-900">
-                    <?= $user["department"] ?>
-                  </div>
-                </td>
-              </tr>
-              <tr>
-                <td class="px-6 py-4 whitespace-nowrap">
-                  <div class="text-ms font-medium text-gray-900">
-                    文理
-                  </div>
-                </td>
-                <td class="px-6 py-4 whitespace-nowrap">
-                  <div class="text-ms font-medium text-gray-900">
-                    <?= $user["division"] ?>
-                  </div>
-                </td>
-              </tr>
-              <tr>
-                <td class="px-6 py-4 whitespace-nowrap">
-                  <div class="text-ms font-medium text-gray-900">
-                    卒業年
-                  </div>
-                </td>
-                <td class="px-6 py-4 whitespace-nowrap">
-                  <div class="text-ms font-medium text-gray-900">
-                    <?= $user["grad_year"] ?>
-                  </div>
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-      </main>
+          <tr>
+            <th scope="row" class="px-2 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+              縦もく
+            </th>
+            <td class="px-6 py-4">
+              <?= $event[0]['tate'] ?>
+            </td>
+
+          </tr>
+          <tr>
+            <th scope="row" class="px-2 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+              横もく
+            </th>
+            <td class="px-6 py-4">
+              <?= $event[0]['yoko'] ?>
+            </td>
+
+          </tr>
+          <tr>
+            <th scope="row" class="px-2 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+              ブラシス
+            </th>
+            <td class="px-6 py-4">
+              <?= $event[0]['brosis'] ?>
+            </td>
+          </tr>
+          <tr>
+            <th scope="row" class="px-2 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+              大学
+            </th>
+            <td class="px-6 py-4">
+              <?= $event[0]['college'] . $event[0]['faculty'] ?>
+            </td>
+          </tr>
+          <tr>
+            <th scope="row" class="px-2 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+              誕生日
+            </th>
+            <td class="px-6 py-4">
+              <?= $event[0]['birthday'] ?>
+            </td>
+          </tr>
+          <tr>
+            <th scope="row" class="px-2 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+              最寄り駅
+            </th>
+            <td class="px-6 py-4">
+              <?= $event[0]['station'] ?>
+            </td>
+          </tr>
+          <tr>
+            <th scope="row" class="px-2 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+              兼サー
+            </th>
+            <td class="px-6 py-4">
+              <?= $posts[0]['circle'] ?>
+            </td>
+          </tr>
+          <tr>
+            <th scope="row" class="px-2 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+              趣味
+            </th>
+            <td class="px-2 py-4">
+              <?= $posts[0]['hobby'] ?>
+            </td>
+          </tr>
+          <tr>
+            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+              ハバズによくいる日時
+            </th>
+            <td class="px-6 py-4">
+              <?= $posts[0]['harborsdate'] ?>
+            </td>
+          </tr>
+          <tr>
+            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+              自己紹介
+            </th>
+            <td class="px-6 py-4">
+              <?= $posts[0]['message'] ?>
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   </div>
 </body>
